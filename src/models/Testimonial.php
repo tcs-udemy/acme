26fe187f74ecd1c7c61f01cc6171f66bdb0e0780
Trevor Sawler <trevor.sawler@gmail.com>
2015-08-22 14:43:35 -0300

    New model relations

--- src/models/Testimonial.php
+++ src/models/Testimonial.php
@@ -5,5 +5,8 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Testimonial extends Eloquent
 {
-    
+    public function user()
+    {
+        return $this->hasOne('Acme\models\User');
+    }
 }
--- src/models/User.php
+++ src/models/User.php
@@ -5,5 +5,8 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class User extends Eloquent
 {
-    
+    public function testimonials()
+    {
+        return $this->hasMany('Acme\models\Testimonial');
+    }
 }
