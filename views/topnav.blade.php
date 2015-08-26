b76305bf09e3c1646baa27f990332b0eb2b98b1f
Trevor Sawler <trevor.sawler@gmail.com>
2015-08-26 10:51:42 -0300

    Top nav improvements

--- views/topnav.blade.php
+++ views/topnav.blade.php
@@ -17,11 +17,33 @@
         <li><a href="/testimonials">Testimonials</a></li>
         @if(Acme\auth\LoggedIn::user())
             <li><a href="/add-testimonial">Add a Testimonial</a></li>
-            <li><a href="/logout">Logout</a></li>
-        @else
-            <li><a href="/login">Login</a></li>
         @endif
-
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+          @if(Acme\auth\LoggedIn::user())
+          <li class="dropdown">
+            <a id="drop1" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              Admin
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="drop1">
+              <li><a href="#">Edit Page</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </li>
+            <li>
+                <a href="/logout">
+                    <span class="glyphicon glyphicon-lock" aria-hidden="true"></span> Logout
+                </a>
+            </li>
+          @else
+            <li>
+                <a href="/login">
+                    <span class="glyphicon glyphicon-lock" aria-hidden="true"></span> Login
+                </a>
+            </li>
+          @endif
       </ul>
     </div><!--/.nav-collapse -->
   </div>
