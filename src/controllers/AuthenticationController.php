3e94f7efc6155595032cdd151c4c7c3746172fac
Trevor Sawler <trevor.sawler@gmail.com>
2015-08-26 12:21:53 -0300

    CSRF protection

--- src/controllers/AuthenticationController.php
+++ src/controllers/AuthenticationController.php
@@ -11,11 +11,19 @@ class AuthenticationController extends BaseController
 
     public function getShowLoginPage()
     {
-        echo $this->blade->render("login");
+        echo $this->blade->render("login", [
+            'signer' => $this->signer,
+        ]);
     }
 
     public function postShowLoginPage()
     {
+
+        if (!$this->signer->validateSignature($_POST['_token'])) {
+            header('HTTP/1.0 400 Bad Request');
+            exit;
+        }
+
         $okay = true;
         $email = $_REQUEST['email'];
         $password = $_REQUEST['password'];
@@ -43,7 +51,9 @@ class AuthenticationController extends BaseController
             exit();
         } else {
             $_SESSION['msg'] = ["Invalid login!"];
-            echo $this->blade->render('login');
+            echo $this->blade->render("login", [
+                'signer' => $this->signer,
+            ]);
             unset($_SESSION['msg']);
             exit();
         }
--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -2,14 +2,17 @@
 namespace Acme\controllers;
 
 use duncan3dc\Laravel\BladeInstance;
+use Kunststube\CSRFP\SignatureGenerator;
 
 class BaseController
 {
 
     protected $blade;
+    protected $signer;
 
     public function __construct()
     {
+        $this->signer = new SignatureGenerator(getenv('CSRF_SECRET'));
         $this->blade = new BladeInstance("/vagrant/views", "/vagrant/cache/views");
     }
 
